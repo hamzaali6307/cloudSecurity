a72a94e5c3ddaafaf966531ae4bdca8ddf0d04d2
hamzaali <hamza.ali6307@gmail.com>
2021-08-08 13:27:51 +0500

    setting up git flow o=in project

--- app/src/main/java/com/hamy/cloudstorage/Fragments/MainView.kt
+++ app/src/main/java/com/hamy/cloudstorage/Fragments/MainView.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import com.hamy.cloudstorage.R
 
-class SplashView : Fragment() {
+class MainView : Fragment() {
     lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
